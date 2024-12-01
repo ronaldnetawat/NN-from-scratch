252273bc24d79ef0c1de769aab069effd1833d72
Ron Netawat <ronnetawat@gmail.com>
2024-12-01 13:13:30 -0500

    Add base_layer file

---
+++ base_layer.py
@@ -0,0 +1,13 @@
+# Writing the Base Layer class
+class Layer:
+    def __init__(self) -> None:
+        self.input = None
+        self.output = None
+
+    # Forward Method: take input and return output
+    def forward(self, input):
+        pass
+
+    # Backward Method: update parameters and return input grad
+    def backward(self, output_gradient, learning_rate):
+        pass
